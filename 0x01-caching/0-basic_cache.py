16fdbfbae5ffac1863df334b9551c753c6a3c267
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-31 08:26:19 +0300

    p

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""implement a caching system"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None

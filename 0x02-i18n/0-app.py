84774f47eff10f2df59db439eb0330b9cbda918a
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-11-05 23:26:18 +0200

    p

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """ Index
+    """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
